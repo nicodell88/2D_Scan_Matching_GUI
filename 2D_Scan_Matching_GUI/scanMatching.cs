a48843bd1fe0e5358829e8bfc2455cdb759b3a9e
Nic O'Dell <nicodell88@gmail.com>
2020-07-03 22:01:48 +1000

    Cost function implemented
    Cost function works, passing two unit tests.

---
+++ 2D_Scan_Matching_GUI/scanMatching.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using MathNet.Numerics.Optimization;
+using MathNet.Numerics.Optimization.TrustRegion;
+
+
+namespace D_Scan_Matching_GUI
+{
+    class SO2
+    {
+        public static Matrix<double> EulerRotation(double Phi)
+        {
+            double c = Math.Cos(Phi);
+            double s = Math.Sin(Phi);
+            double[] R = { c, s, -s, c };
+            return Matrix<double>.Build.DenseOfColumnMajor(2, 2, R);
+        }
+
+        public static Matrix<double> Derivative(double Phi)
+        {
+            double[] s = { 0, 1, -1, 0 };
+            var S = Matrix<double>.Build.DenseOfColumnMajor(2, 2, s);
+            return S.Multiply(EulerRotation(Phi));
+        }
+    }
+
+    public static class ScanMatching
+    {
+
+        private static Matrix<double> _rPNn = Matrix<double>.Build.Dense(2, 1);
+        private static Matrix<double> _rEBb = Matrix<double>.Build.Dense(2, 1);
+        private static readonly double Sigma = 1;
+
+        public static (double, Vector<double>, Matrix<double>) Cost(Vector<double> v)
+        //public static Vector<double> cost()
+        {
+            //var f = new Func<Vector<double>, double>(v => Math.Pow(v[0], 2) + Math.Pow(v[1], 4) + Math.Pow(v[2], 6));              // define function
+            //var g = new Func<Vector<double>, Vector<double>>(v => new DenseVector(new[] { 2.0 * v[0], 4.0 * v[1], 6.0 * v[2] }));  // define grandient
+            //var obj = ObjectiveFunction.Gradient(f, g);
+            //var solver = new TrustRegionNewtonCGMinimizer(1e-5, 1e-5, 1e-5, 1000);
+            //var result = solver.FindMinimum((IObjectiveModel)obj, new DenseVector(new[] { 15.0, 15.0, 15.0 })); // initial estimate = (15,15,15)
+
+            //IObjectiveFunction GradientHessian(Func<Vector<double>, double> function, Func<Vector<double>, Vector<double>> gradient, Func<Vector<double>, Matrix<double>> hessian)
+
+            double f = 0;
+            var G = Vector<double>.Build;
+            var H = Matrix<double>.Build;
+
+            var g = G.Dense(3);
+            var h = H.Dense(3, 3);
+
+
+            //var rENn = Matrix<double>.Build.Dense(2,1);
+
+            var rBNn = v.SubVector(0, 2);
+            var rEBn = SO2.EulerRotation(v.At(2)).Multiply(_rEBb);
+            var rENn = Matrix<double>.Build.Dense(2, rEBn.ColumnCount);
+            for (int i = 0; i < rENn.ColumnCount; i++)
+            {
+                rENn.SetColumn(i, rBNn.Add(rEBn.Column(i)));
+            }
+
+            //Get point cloud data.
+            for (int i = 0; i < _rEBb.ColumnCount; i++)
+            {
+                for (int j = 0; j < _rPNn.ColumnCount; j++)
+                {
+                    var e = rENn.Column(i).Subtract(_rPNn.Column(j));
+
+                    f += 1.0 / Math.Sqrt(2 * Math.PI * Sigma * Sigma) * Math.Exp(-1.0 / (2 * Sigma * Sigma) * e.DotProduct(e));
+                }
+            }
+
+
+
+
+
+            return (-f, g, h);
+        }
+
+
+
+
+        public static Matrix<double> rPNn
+        {
+            get { return _rPNn; }
+            set { _rPNn = value; }
+        }
+
+        public static Matrix<double> rEBb
+        {
+            get { return _rEBb; }
+            set { _rEBb = value; }
+        }
+
+
+
+        public static void Init(String inpF)
+        {
+            //Read data from file
+
+            // Is this a good way to store the point cloud data?
+            //List<Matrix<double>> Foobar= new List<Matrix<double>>();
+
+            //Parse data
+
+            //Initialise reference point cloud, expressed in map coordinates. rPNn
+
+            //Initialise independent point cloud, expressed in body coordinates.rPBb
+
+            //
+
+        }
+
+
+
+    }
+
+
+
+}
+
+
---
+++ Scan_Matching_GUI.test/TestCostFunction.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using D_Scan_Matching_GUI;
+using Xunit;
+
+using MathNet.Numerics.LinearAlgebra;
+
+namespace Scan_Matching_GUI.test
+{
+    public class TestCostFunction
+    {
+        [Fact]
+        public void CloserPointsImproveCost()
+        {
+            // Initialise point clouds
+            double[] x1 = { 0, 1 };
+            double[] x2 = { 0, 2 };
+
+            Matrix<double> PointCloud1 = Matrix<double>.Build.DenseOfColumnMajor(2,1,x1);
+            Matrix<double> PointCloud2 = Matrix<double>.Build.DenseOfColumnMajor(2,1,x2);
+
+            ScanMatching.rEBb = PointCloud1;
+            ScanMatching.rPNn = PointCloud2;
+
+            // Generate two pose vectors (x,y,psi)
+            Vector<double> v1 = Vector<double>.Build.Dense(3);//This should yield a higher cost than    
+            double[] vv = {0,1,0};
+            Vector<double> v2 = Vector<double>.Build.DenseOfArray(vv);//this one
+
+            (double CostExpectLow,  var G1, var H1 )= ScanMatching.Cost(v2);
+            (double CostExpectHigh, var G2, var H2) = ScanMatching.Cost(v1);
+
+            // Since the points lie on top of one another when pose = {0 1 0} that should yield a lower cost
+            
+
+            Assert.True(CostExpectLow < CostExpectHigh,"Expected closer points to yield a lower cost, something is fundamentally wrong with the cost function");
+
+        }
+        [Fact]
+        /*The following scenarios are contrived such that the point cloud locations are mirrored and should produce the same cost*/
+        public void EqualCostScenarios()
+        {
+            // Initialise point clouds
+            double[] x1 = { 0, 0, 0, 1 };
+            double[] x2 = { 0, -0.5, 0, 0.5 };
+
+            Matrix<double> PointCloud1 = Matrix<double>.Build.DenseOfColumnMajor(2, 2, x1);
+            Matrix<double> PointCloud2 = Matrix<double>.Build.DenseOfColumnMajor(2, 2, x2);
+
+            ScanMatching.rEBb = PointCloud2;
+            ScanMatching.rPNn = PointCloud1;
+
+            // Generate two pose vectors (x,y,psi)
+            double[] vv1 = { -0.25, 0.5, -0.1 };
+            Vector<double> v1 = Vector<double>.Build.Dense(vv1);
+
+            double[] vv2 = { 0.25, 0.5, 0.1 };
+            Vector<double> v2 = Vector<double>.Build.DenseOfArray(vv2);
+
+            (double expected, var G1, var H1) = ScanMatching.Cost(v2);
+            (double actual, var G2, var H2) = ScanMatching.Cost(v1);
+
+            /*Precision is given as an integer defining the number of decimal places, 
+             * Normal practice would be to select precision based on machine precision
+             * E.g., sqrt(eps) where eps is machine precision, sqrt(eps) = 8.5e-8 on my machine.
+             * Therefore, 8 decimal places will suffice
+             */
+            Assert.Equal(expected, actual, 8); 
+        }
+    }
+}
